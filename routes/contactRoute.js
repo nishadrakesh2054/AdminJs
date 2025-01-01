f8a4ff73c95908e8bd1b7455516c7d875f43dc6c
Rakesh Kumar Sahani <nishadrakesh2054@gmail.com>
2025-01-01 17:24:22 +0545

    thrusday

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -3,13 +3,11 @@ import express from "express";
 import {
   getContacts,
   createContact,
-  deleteContact,
 } from "../controllers/contactController.js";
 
 const router = express.Router();
 
 // Routes for CRUD operations on contacts
 router.route("/").get(getContacts).post(createContact);
-router.route("/:id").delete(deleteContact);
 
 export default router;
